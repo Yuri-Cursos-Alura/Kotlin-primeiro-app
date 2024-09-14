57b25cfdb57f16db7fb729aff5c80c435d5b999f
yuri-almir-pinto <yurialmirp@gmail.com>
2024-09-14 18:07:16 -0300

    Alterar classe Game para uma data class

--- src/main/kotlin/com/yuri_kotlin_learning/models/Game.kt
+++ src/main/kotlin/com/yuri_kotlin_learning/models/Game.kt
@@ -2,7 +2,7 @@ package com.yuri_kotlin_learning.models
 
 import com.yuri_kotlin_learning.services.ApiGame
 
-class Game(val title: String, val thumb: String, val description: String) {
+data class Game(val title: String, val thumb: String, val description: String) {
     companion object {
         fun from(apiGame: ApiGame, description: String): Game {
             return Game(apiGame.info.title, apiGame.info.thumb, description)
